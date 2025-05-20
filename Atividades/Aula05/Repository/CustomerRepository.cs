1d5f07d6f4d03db7a786bd89521844aa8dd8ac5a
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2025-05-19 22:06:21 -0300

    Estrutura POO

---
+++ Atividades/Aula05/Repository/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CustomerRepository
+    {
+        public Customer Retrieve()
+        {
+            return new Customer();
+        }
+
+        public void Save(Customer customer)
+        {
+        }
+    }
+}
