b0564a57b3a0b52a4016fbf7cc2537660118342d
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2025-05-26 21:38:38 -0300

    Customer - Repository e Model - Post e Get em Lista

--- Atividades/Aula05/Repository/CustomerRepository.cs
+++ Atividades/Aula05/Repository/CustomerRepository.cs
@@ -1,21 +1,66 @@
 ﻿using Modelo;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository
 {
     public class CustomerRepository
     {
-        public Customer Retrieve()
+        public Customer Retrieve(int id)
         {
-            return new Customer();
+            foreach ( Customer c in CustomerData.Customers )            
+                if (c.Id == id)                
+                    return c;                           
+
+            return null!;
+        }
+
+        public List<Customer> RetrieveByName(string name)
+        {
+            List<Customer> ret = new List<Customer>();
+
+            foreach (Customer c in CustomerData.Customers)            
+                if (c.Name!.ToLower().Contains(name.ToLower()))                
+                    ret.Add(c);                            
+
+            return ret;
+        }
+
+        public List<Customer> RetrieveAll()
+        {
+            return CustomerData.Customers;
         }
 
         public void Save(Customer customer)
         {
+            customer.Id = GetCount() + 1;
+            CustomerData.Customers.Add(customer);
+        }
+
+        public bool Delete(Customer customer)
+        {
+            return CustomerData.Customers.Remove(customer);
+        }
+
+        public bool DeleteById(int id)
+        {
+            Customer customer = Retrieve(id);
+
+            if(customer != null)
+                return Delete(customer);
+
+            return false;
+        }
+
+        public void Update(Customer newCustomer)
+        {
+            Customer oldCustomer = Retrieve(newCustomer.Id);
+            oldCustomer.Name = newCustomer.Name;
+            oldCustomer.WorkAddress = newCustomer.WorkAddress;
+            oldCustomer.HomeAddress = newCustomer.HomeAddress;
+        }
+
+        public int GetCount()
+        {
+            return CustomerData.Customers.Count;
         }
     }
 }
