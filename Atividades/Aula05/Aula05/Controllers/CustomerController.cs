21e6c1d7602aadd6af3f06f8cfea5bb8b75c46a3
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2025-06-02 21:20:45 -0300

    Arquivo delimitado

--- Atividades/Aula05/Aula05/Controllers/CustomerController.cs
+++ Atividades/Aula05/Aula05/Controllers/CustomerController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Modelo;
 using Repository;
+using System.IO;
 
 namespace Aula05.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly IWebHostEnvironment environment;
+
         private CustomerRepository _customerRepository;
 
-        public CustomerController()
+        public CustomerController(
+            IWebHostEnvironment environment
+        )
         {
             _customerRepository = new CustomerRepository();
+            this.environment = environment;
         }
 
         [HttpGet]
@@ -36,6 +42,40 @@ namespace Aula05.Controllers
         [HttpGet]
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ExportDelimitatedFile()
+        {
+            string fileContent = string.Empty;
+            foreach (Customer c in CustomerData.Customers)
+            {
+                fileContent += 
+                    $"{c.Id};{c.Name};{c.HomeAddress!.Id};{c.HomeAddress.City};{c.HomeAddress.State};{c.HomeAddress.Country};{c.HomeAddress.Street1};{c.HomeAddress.Street2};{c.HomeAddress.PostalCode};{c.HomeAddress.AddressType}\n";
+            }
+
+            var path = Path.Combine(
+                environment.WebRootPath,
+                "TextFiles"
+            );
+
+            if (!System.IO.Directory.Exists(path))
+                System.IO.Directory.CreateDirectory(path);
+
+            var filepath = Path.Combine(
+                path, 
+                "Delimitado.txt"
+            );            
+
+            if (!System.IO.File.Exists(filepath))
+            {                
+                using (StreamWriter sw = System.IO.File.CreateText(filepath))
+                {
+                    sw.Write(fileContent);
+                }
+            }
+
             return View();
         }
     }
